c8de61240b3cebf94c9c553a103cb6007b1157c5
Will Lunden <lundenwill@gmail.com>
2025-02-22 08:56:23 -0800

    fix: update button, cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script>
 	import { GradientButton } from 'flowbite-svelte';
-	import { page } from '$app/stores';
 	// @ts-ignore
 	import Navbar from './Navbar.svelte';
 	import Typewriter from './Typewriter.svelte';
 	import Socialbar from './Socialbar.svelte';
-	$: activeUrl = $page.url.pathname;
 	import '@fortawesome/fontawesome-free/css/all.min.css';
 
-	// Put your paragraphs into an array
+
 	const paragraphs = [
 		'Hi there.',
 		'I am a technical professional with over 15 years of experience in physics research, AI/ML, and software development. Currently, I lead technical development at Delfi Labs as CTO.',
@@ -16,11 +14,11 @@
 		'Feel free to take a look around.'
 	];
 
-	// Which paragraph is currently being typed?
+
 	let currentParagraph = 0;
 	let showButton = false;
 
-	// Increment when we get the 'done' event from Typewriter
+
 	function handleDone() {
 		if (currentParagraph < paragraphs.length - 1) {
 			setTimeout(() => {
@@ -39,24 +37,19 @@
 
 <Navbar activePage="home" />
 
-<!-- MAIN CONTENT CENTERED -->
+
 <div class="m-6 flex min-h-screen flex-col items-center justify-center text-center">
 	<h1 class="mb-4 text-4xl my-name ">
 		<i class="fa fa-flask" aria-hidden="true"></i>
 		Will Lunden, PhD
 	</h1>
-	<!-- Loop through paragraphs and only start typing the next one when the previous is done -->
 	{#each paragraphs as paragraph, index}
 		{#if index <= currentParagraph}
-			<!-- 
-      Each Typewriter will show once we’re at or past its index. 
-      on:done calls handleDone() so the next paragraph can start. 
-    -->
 			<Typewriter text={paragraph} on:done={handleDone} />
 		{/if}
 	{/each}
 	{#if showButton}
-		<GradientButton color="pinkToOrange" class="mt-4 mb-4">More About Me</GradientButton>
+		<GradientButton color="pinkToOrange" class="mt-4 mb-4" href='/about-me'>More About Me</GradientButton>
 	{/if}
 </div>
 
